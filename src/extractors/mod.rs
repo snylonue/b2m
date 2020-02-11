25c5fcfe9d284121962e763c63b0e3c1896696de
snylonue <snylonue@gmail.com>
2020-02-11 16:15:44 +0800

    add trait Parser and lots of macros It will help use multiple parsers

--- src/extractors/mod.rs
+++ src/extractors/mod.rs
@@ -1,13 +1,70 @@
+#[macro_export]
+macro_rules! matched {
+    ($url:expr, $reg: expr) => {
+        {
+            let reg = regex::Regex::new($reg).unwrap();
+            reg.is_match($url)
+        }
+    };
+    ($url:expr, $($regs: expr),*) => {
+        
+        {
+            let regs = regex::RegexSet::new(&[
+                $($regs,)*
+            ]).unwrap();
+            regs.is_match($url)
+        }
+    };
+}
+#[macro_export]
+macro_rules! value_to_string {
+    ($v: expr) => {
+        match $v {
+            serde_json::Value::String(ref s) => Some(s.clone()),
+            _ => None,
+        }
+    };
+    ($v: expr, $or: expr) => {
+        match $v {
+            serde_json::Value::String(ref s) => Some(s.clone()),
+            _ => $crate::value_to_string!($or),
+        }
+    };
+}
+#[macro_export]
+macro_rules! parse_json {
+    ($s: expr) => {
+        match serde_json::from_str($s) {
+            Ok(v) => v,
+            Err(e) => return Err(failure::err_msg(format!("Failed to deserialize: {}", e))),
+        }
+    };
+    ($s: expr, $err_msg: expr) => {
+        match serde_json::from_str($s) {
+            Ok(v) => v,
+            Err(_) => return Err($err_msg),
+        }
+    };
+}
+
 pub mod bilibili;
 pub mod iqiyi;
 
 use serde_json::Value;
+use std::process;
+use super::Res;
+
+type ResultInfo = super::Res<super::MediaInfo>;
 
 pub struct Url {
     pub videos: Vec<String>,
     pub audios: Vec<String>,
 }
 
+pub trait Parser {
+    fn is_support(url: &str)  -> bool;
+    fn parse(url: &str) -> ResultInfo;
+}
 impl Url {
     pub fn new(videos: Vec<String>, audios: Vec<String>) -> Self {
         Url { videos, audios }
--- src/lib.rs
+++ src/lib.rs
@@ -1,97 +1,27 @@
+macro_rules! find_parser {
+    ($url: expr, $site: ident, $parser: ident) => {
+        if $crate::extractors::$site::$parser::is_support($url) {
+            return $crate::extractors::$site::$parser::parse($url);
+        }
+    };
+}
+
 pub mod extractors;
 pub mod cmd;
 
-use serde_json;
-use serde_json::Value;
 use failure::err_msg;
 use std::process;
 use extractors::Url;
-
-#[macro_export]
-macro_rules! value_to_string {
-    ($v: expr) => {
-        match $v {
-            serde_json::Value::String(ref s) => Some(s.clone()),
-            _ => None,
-        }
-    };
-    ($v: expr, $or: expr) => {
-        match $v {
-            serde_json::Value::String(ref s) => Some(s.clone()),
-            _ => value_to_string!($or),
-        }
-    };
-}
-macro_rules! parse_json {
-    ($s: expr) => {
-        match serde_json::from_str($s) {
-            Ok(v) => v,
-            Err(e) => return Err(err_msg(format!("Failed to deserialize: {}", e))),
-        }
-    };
-    ($s: expr, $err_msg: expr) => {
-        match serde_json::from_str($s) {
-            Ok(v) => v,
-            Err(_) => return Err($err_msg),
-        }
-    };
-}
+use extractors::Parser;
 
 type Res<T> = Result<T, failure::Error>;
 
-pub enum Parser {
-    YouGet,
-    Annie,
-}
 pub struct MediaInfo {
     pub url: Url,
     pub title: Option<String>,
     pub referrer: Option<String>,
 }
 
-impl Parser {
-    pub fn parse(&self, url: &str) -> Res<MediaInfo> {
-        match *self {
-            Parser::YouGet => {
-                let (stdout, _) = cmd::run_command(process::Command::new("you-get")
-                    .arg(url)
-                    .arg("--json")
-                    .stderr(process::Stdio::null())
-                )?;
-                let json_stdout = parse_json!(&stdout);
-                Self::you_get(&json_stdout)
-            },
-            Parser::Annie => {
-                let (stdout, _) = cmd::run_command(process::Command::new("annie")
-                    .arg("-j")
-                    .arg(url)
-                )?;
-                let json_stdout = parse_json!(&stdout);
-                Self::annie(&json_stdout)
-            }
-        }
-    }
-    fn you_get(json: &Value) -> Res<MediaInfo> {
-        let url = match extractors::parse_you_get_url(&json) {
-            Some(el) => el,
-            None => return Err(err_msg("Failed to parse stdout as url")),
-        };
-        // referrer = json['extra']['referer'] || json_output['url']
-        let referrer = value_to_string!(json["extra"]["referer"], json["url"]);
-        // title = json['title']
-        let title = value_to_string!(json["title"]);
-        Ok(MediaInfo { url, referrer, title })
-    }
-    fn annie(json: &Value) -> Res<MediaInfo> {
-        let url = match extractors::parse_annie_url(&json) {
-            Some(el) => el,
-            None => return Err(err_msg("Failed to parse stdout as url")),
-        };
-        let referrer = value_to_string!(json["url"]);
-        let title = value_to_string!(json["title"]);
-        Ok(MediaInfo { url, referrer, title })
-    }
-}
 impl MediaInfo {
     pub fn play(&self) -> Res<()> {
         let Url { videos, audios } = &self.url;
