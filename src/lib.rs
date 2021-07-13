1724a93c99c8c51c0c0239849cd363839c944afd
snylonue <snylonue@gmail.com>
2021-07-13 21:34:53 +0800

    main, lib, check: get rid of external programs and old structs
    temporarily

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,7 @@ pub mod proxy;
 use crate::cli::Config;
 use crate::parsers::Url;
 use anyhow::Result;
+use finata::{Finata, Track};
 use std::io::Result as IoResult;
 use std::process::Command;
 
@@ -122,4 +123,46 @@ impl MediaInfo {
         cmd.arg("--no-ytdl");
         cmd
     }
+}
+
+pub fn spwan_command(playlist: Finata, config: &Config) -> Command {
+    let mut cmd = Command::new("mpv");
+    let orig = &playlist.raws()[0];
+    let videos: Vec<_> = orig
+        .tracks
+        .iter()
+        .filter(|t| matches!(t, Track::Video(_)))
+        .collect();
+    let audios = orig.tracks.iter().filter(|t| matches!(t, Track::Audio(_)));
+    match videos.len() {
+        0 => cmd.args(audios.map(|a| a.as_url().to_string())),
+        1 => cmd
+            .arg(videos[0].as_url().to_string())
+            .args(audios.map(|a| format!("--audio-file={}", a.as_url().to_string()))),
+        _ => cmd
+            .args(videos.iter().map(|v| v.as_url().to_string()))
+            .args(audios.map(|a| format!("--audio-file={}", a.as_url().to_string())))
+            .args::<&[&str], _>(if config.merge {
+                &["--merge-files"]
+            } else {
+                &[]
+            }),
+    };
+    cmd.arg(playlist.title()).arg("--no-ytdl");
+    if config.no_audio {
+        cmd.args(&["--ao=null", "--no-audio"]);
+    }
+    if config.no_video {
+        cmd.args(&["--no-video", "--force-window=immediate"]);
+    }
+    if let Some(proxy) = &config.proxy {
+        cmd.env("HTTP_PROXY", proxy.to_string());
+    }
+    if let Some(cookie) = &config.cookie {
+        cmd.arg(format!("--cookies-file={}", cookie));
+    }
+    if let Some(values) = config.commands.clone() {
+        cmd.args(values);
+    }
+    cmd
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,4 @@
-macro_rules! find_parser {
-    ($url: expr, $setting: expr, $($site: ident, $extractor_name: expr, $extractor: ident),*) => {
-       {
-            use $crate::extractors::Extractor;
-            $(#[cfg(feature = $extractor_name)]
-            if $setting.parser.map(|p| p == $extractor_name).unwrap_or(true)
-                && $crate::extractors::$site::$extractor::is_support($url) {
-                match $crate::extractors::$site::$extractor::extract($url, $setting) {
-                    res @ Ok(_) => return res,
-                    Err(e) => {
-                        eprintln!("Error caught with {}/{}", stringify!($site), $extractor_name);
-                        eprintln!("Error: {:#?}", e);
-                     }
-                }
-            })*
-        }
-    };
-}
+#[allow(unused_macros)]
 macro_rules! features {
     ($($feature: tt),*) => {
         println!("features enabled:");
@@ -27,9 +10,16 @@ macro_rules! features {
 mod check;
 
 use anyhow::Result;
-use serde_json::json;
+use finata::Extract;
+use finata::ExtractSync;
 
 use b2m::*;
+use finata::website::bilibili::Bangumi;
+use finata::website::bilibili::Video;
+use finata::website::netease_music::PlayList;
+use finata::website::netease_music::Song;
+use finata::website::pixiv::Collection;
+use finata::website::pixiv::Pixiv;
 
 fn main() -> Result<()> {
     let matches = cli::b2m().get_matches();
@@ -38,71 +28,39 @@ fn main() -> Result<()> {
         check(&config);
         return Ok(());
     }
-    let media = parse(config.url, &config)?;
-    if config.info {
-        print_info(media, config.json);
-        return Ok(());
-    }
-    Ok(media.play(&config)?)
+    let mut extractor = find_extractor(config.url)?;
+    let res = extractor.extract_sync()?;
+    spwan_command(res, &config).spawn()?;
+    Ok(())
 }
 fn check(conf: &cli::Config) {
     println!("Running check");
     println!("b2m version: {}\n", cli::VERSION);
-    features!("annie", "nfinata", "youget");
     dbg!(conf);
-    if check::check_you_get() {
-        println!("\nyou-get check succeeded");
-    } else {
-        println!("\nyou-get check failed");
-    }
-    println!();
-    if check::check_annie() {
-        println!("\nannie check succeeded");
-    } else {
-        println!("\nannie check failed");
-    }
-    println!();
     if check::check_mpv() {
         println!("\nmpv check succeeded");
     } else {
         println!("\nmpv check failed");
     }
 }
-fn print_info(media: MediaInfo, json: bool) {
-    let MediaInfo {
-        url: parsers::Url { videos, audios },
-        title,
-        referrer,
-        user_agent,
-    } = media;
-    if json {
-        let j = json!({
-            "video": videos,
-            "audio": audios,
-            "title": title,
-            "referrer": referrer,
-            "user-agent": user_agent,
-        });
-        println!("{}", serde_json::to_string_pretty(&j).unwrap());
-    } else {
-        println!("video: {}", serde_json::to_string(&videos).unwrap());
-        println!("audio: {}", serde_json::to_string(&audios).unwrap());
-        println!("title: {}", title.unwrap_or_else(String::new));
-        println!("referrer: {}", referrer.unwrap_or_else(String::new));
-        println!("user-agent: {}", user_agent.unwrap_or_else(String::new));
+fn find_extractor(url: &str) -> Result<Box<dyn Extract>> {
+    if let Ok(extr) = Video::new(url) {
+        return Ok(Box::new(extr));
     }
-}
-#[rustfmt::skip]
-pub fn parse(url: &str, setting: &cli::Config) -> Result<MediaInfo> {
-    find_parser!(
-        url, setting,
-        bilibili, "nfinata", Finata,
-        bilibili, "annie", Annie,
-        bilibili, "youget", YouGet,
-        youtube, "annie", Annie,
-        iqiyi, "annie", Annie,
-        iqiyi, "youget", YouGet,
-        netease_music, "nfinata", Finata
-    );
-    Err(anyhow::anyhow!("Unsupport url"))
+    if let Ok(extr) = Bangumi::new(url) {
+        return Ok(Box::new(extr));
+    }
+    if let Ok(extr) = Song::new(url.parse()?) {
+        return Ok(Box::new(extr));
+    }
+    if let Ok(extr) = PlayList::new(url.parse()?) {
+        return Ok(Box::new(extr));
+    }
+    if let Ok(extr) = Pixiv::new(url) {
+        return Ok(Box::new(extr));
+    }
+    if let Ok(extr) = Collection::new(url) {
+        return Ok(Box::new(extr));
+    }
+    Err(anyhow::anyhow!("Unsupported url"))
 }
